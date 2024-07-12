f984406fcff661e55f28e5f06b21b6571d40cc6f
peyton brimmer <peytonbrimmer0@gmail.com>
2024-07-12 13:25:42 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# JATE Text Editor Project
+
+## Description
+
+Pogressive web app used as a text editor. This text editor was built with indexedDB, express.js and webpack. 
+
+## Installation
+first start with cloning the repository
+run npm i or npm install on the terminal 
+next use npm run start in the terminal
+navigate to https://localhost:3000
+
+## Usage
+
+
+## Screenshot
+
+
+## License
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
---
+++ client/webpack.config.js
@@ -0,0 +1,77 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      // webpack plugin to generate an HTML file to serve the bundle
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        title: 'JATE',
+        favicon: 'favicon.ico',
+      }),
+      // webpack plugin to generate a manifest file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'just another text editor',
+        short_name: 'JATE',
+        description: 'A simple text editor',
+        background_color: '#01579b',
+        theme_color: '#ffffff',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/assets/icons/icon-192x192.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+    ],
+
+// CSS loaders and babel to webpack.
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/transform-runtime',
+              ],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
